dd9bc7342e95f12cdad5b6c3d89a797714404567
Nancy Lambert-Brown <2643160+n-lambert@users.noreply.github.com>
2020-09-19 20:17:02 -0700

    added CRUD functionality

--- server.js
+++ server.js
@@ -5,13 +5,19 @@ const fs = require("fs");
 const app = express();
 let PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'db')));
 app.use(express.static(path.join(__dirname, 'routes')));
 
 //routes
+// api routes
 require("./routes/apiRoutes")(app);
+// html routes
 require("./routes/htmlRoutes")(app);
+
+
 
 app.listen(PORT, function() {
     console.log('listening on port ' + PORT);
