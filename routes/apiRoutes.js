1185d68ba89c0609ce5286a5238c24f5256d32b7
Nancy Lambert-Brown <2643160+n-lambert@users.noreply.github.com>
2020-09-19 20:17:27 -0700

    moved routes to separate directory

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,51 @@
-var path = require("path");
-var fs = require("fs");
-var notesData = require("../db/db.json");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'db')));
+app.use(express.static(path.join(__dirname, 'routes')));
 
 //Routing
 
 module.exports = function(app){
-app.get('/api/notes', function (request, response) {
-    // get all body stuff from request.body object
-    // get all request parameter stuff from request.params object
+    app.get('/api/notes', function (request, response) {
+        // respond with: content of db.json
+        return response.json(JSON.parse(fs.readFileSync('./db/db.json')));
+    });
     
-    // respond with: response.send()
-
-});
-
-app.post('/api/notes', function (request, response) {
-    // get all body stuff from request.body object
-    // get all request parameter stuff from request.params object
+    app.post('/api/notes', function (request, response) {
+        // get all body stuff from request.body object
+        // get all request parameter stuff from request.params object
+        let newNote = request.body;
+        let existingNotes = JSON.parse(fs.readFileSync('./db/db.json'));
+        let id = 1;
+        while
+            (existingNotes.some((value, index) => {
+                return value.id === id
+            })) {
+            id++
+        }
+        newNote.id = id;
+        existingNotes.push(newNote);
+        console.log("adding new note");
+        fs.writeFileSync('./db/db.json', JSON.stringify(existingNotes));
+    });
     
-    // respond with: response.send()
-
-});
-
-app.delete('/api/notes/:id', function (request, response) {
-    // get all body stuff from request.body object
-    // get all request parameter stuff from request.params object
+    app.delete('/api/notes/:id', function (request, response) {
+        // get all body stuff from request.body object
+        // get all request parameter stuff from request.params object
+        let existingNotes = JSON.parse(fs.readFileSync('./db/db.json'));
+        let id = parseInt(request.params.id);
+        console.log("deleting note with id: " + id);
+        existingNotes = existingNotes.filter(note => {
+            return note.id !== id
+        });
+        console.log("updated notes are now: " + existingNotes);
+        fs.writeFileSync('./db/db.json', JSON.stringify(existingNotes));
     
-    // respond with: response.send()
-
-});
-
-
+    });
 }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,26 +1,32 @@
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'db')));
+app.use(express.static(path.join(__dirname, 'routes')));
 //Routing
 
-module.exports = function(app){
-
-app.get('/notes', function (request, response) {
-    // get all body stuff from request.body object
-    // get all request parameter stuff from request.params object
+module.exports = function(app){ 
+    app.get('/notes', function (request, response) {
+        // get all body stuff from request.body object
+        // get all request parameter stuff from request.params object
+        
+        // respond with: response.send()
+        response.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
     
-    // respond with: response.send()
-    response.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-
-//anything other than notes gets served the index page
-app.get('*', function (request, response) {
-    // get all body stuff from request.body object
-    // get all request parameter stuff from request.params object
+    //anything other than notes gets served the index page
+    app.get('*', function (request, response) {
+        // get all body stuff from request.body object
+        // get all request parameter stuff from request.params object
+        
+        // respond with: response.send()
+        response.sendFile(path.join(__dirname, "../public/index.html"));
     
-    // respond with: response.send()
-    response.sendFile(path.join(__dirname, "../public/index.html"));
-
-});
-
+    });
 }
 
